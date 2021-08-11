767c4c540e897e50777820875729892d7eb3517e
davidperjac <diperez@espol.edu.ec>
2021-08-10 23:56:58 -0500

    modificaciones de errores en clase

--- 1 SEMANA/flores.py
+++ 1 SEMANA/flores.py
@@ -6,28 +6,28 @@ def calculaTotales(categoria) :
   file.readline() 
   dic = {}
   for linea in file:
-    comp,vend,prod,unidades,ventas,fecha = linea.split(",").strip()
+    comp,vend,prod,unidades,ventas,fecha = linea.strip().split(",")
     
     dic[ (comp,vend,prod) ] = dic.get((comp,vend,prod),0)
     dic[(comp,vend,prod)]+=float(ventas)
   file.close()
   return dic
 
-#prueba = calculaTotales("flores.txt")
-#print(prueba)
+prueba = calculaTotales("1 SEMANA/flores.txt")
+print(prueba)
 
 # UNIDAD 7 COLECCIONES
 
 def informacionCovid(archivo) : 
-  file = open("infectados.txt", "r")
+  file = open(archivo, "r")
   file.readline()
   dic = {}
   for linea in file:
-    pais,infec,muertos,continente = linea.split(",").strip()
+    pais,infec,muertos,continente = linea.strip().split(",")
     dic[continente] = dic.get(continente,{})
     dic[continente][pais] = dic[continente].get(pais,{'infectados':infec,'fallecidos':muertos})
   file.close()
   return dic
 
-prueba2 = informacionCovid("infectados.txt")
-print(prueba2)
+prueba2 = informacionCovid("1 SEMANA/infectados.txt")
+#print(prueba2)
